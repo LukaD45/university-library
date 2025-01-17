0ee0df9c9ff0c41e268bc1db79b228b5e079d663
LukaD45 <luka.dusko44@gmail.com>
2025-01-17 09:00:36 +0100

    book cover component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import BookList from "@/components/book-list";
 import BookOverview from "@/components/book-overview";
+import { sampleBooks } from "../constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
-      <BookList />
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
