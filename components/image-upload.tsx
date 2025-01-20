4c6e1b6dee209e3437d7f6ecbdb70b054090d72d
LukaD45 <luka.dusko44@gmail.com>
2025-01-20 14:07:15 +0100

    image upload ikproviders

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,5 +1,50 @@
+"use client";
+
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import { useRef, useState } from "react";
+
+const {
+  env: {
+    imageKit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status: ${response.status} - ${errorText}`
+      );
+    }
+    const data = await response.json();
+    const { signature, expire, token } = data;
+
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Failed to authenticate with ImageKit: ${error.message}`);
+  }
+};
+
 const ImageUpload = () => {
-  return <div>Image upload</div>;
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{
+    filePath: string;
+  } | null>(null);
+
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload className="hidden" ref={ikUploadRef} />
+    </ImageKitProvider>
+  );
 };
 
 export default ImageUpload;
--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,10 @@
 export const config = {
   env: {
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     imageKit: {
-      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-      privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.IMAGE_KIT_PRIVATE_KEY!,
     },
   },
 };
