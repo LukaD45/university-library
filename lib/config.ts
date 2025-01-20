ebddb87023b7a8ede776012138e60c63ff137016
LukaD45 <luka.dusko44@gmail.com>
2025-01-20 13:01:34 +0100

    config.ts in lib folder

---
+++ lib/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  env: {
+    imageKit: {
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+      privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+    },
+  },
+};
+
+export default config;
