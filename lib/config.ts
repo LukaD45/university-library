532428b2b35aa4457070b2c4dc6819b0860f77b2
LukaD45 <luka.dusko44@gmail.com>
2025-01-20 14:59:34 +0100

    db setup

--- lib/config.ts
+++ lib/config.ts
@@ -6,6 +6,9 @@ export const config = {
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
       privateKey: process.env.IMAGE_KIT_PRIVATE_KEY!,
     },
+    database: {
+      url: process.env.DATABASE_URL!,
+    },
   },
 };
 
