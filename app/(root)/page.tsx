4719efc77f0747b968181d90bc444a02a94e032f
LukaD45 <luka.dusko44@gmail.com>
2025-01-21 07:17:05 +0100

    auth.ts configuation of callbacks and session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 import BookList from "@/components/book-list";
 import BookOverview from "@/components/book-overview";
 import { sampleBooks } from "../constants";
+import { users } from "@/db/schema";
+import { db } from "@/db/drizzle";
 
-const Home = () => {
+const Home = async () => {
+  const result = await db.select().from(users);
+
+  console.log(JSON.stringify(result, null, 2));
+
   return (
     <>
       <BookOverview {...sampleBooks[0]} />
