945cc35b5f95e2ac9ff46019223569996e2d88b3
LukaD45 <luka.dusko44@gmail.com>
2025-01-22 05:01:07 +0100

    getInitials function

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,7 @@ const Layout = async ({ children }: LayoutProps) => {
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,10 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Session } from "next-auth";
 
-const Header = () => {
+const Header = ({ session }: { session: Session }) => {
   const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
@@ -22,6 +24,15 @@ const Header = () => {
             href="/library"
           >
             Library
+          </Link>
+        </li>
+        <li>
+          <Link href="/my-profile">
+            <Avatar>
+              <AvatarFallback className="text-white">
+                {session.user?.name}
+              </AvatarFallback>
+            </Avatar>
           </Link>
         </li>
       </ul>
