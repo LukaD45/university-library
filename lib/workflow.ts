07397fc803e95174f39b85b91f79dd05539380fe
LukaD45 <luka.dusko44@gmail.com>
2025-01-24 09:55:09 +0100

    prepearing email setup

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,12 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient } from "@upstash/qstash";
 import { config } from "./config";
 
 export const workflow = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QStashClient({
   token: config.env.upstash.qstashToken,
 });
