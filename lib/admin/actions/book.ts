372e887e10f826129a168e468e2684e4eb49eb97
LukaD45 <luka.dusko44@gmail.com>
2025-01-25 07:01:44 +0100

    book.ts action

--- lib/admin/actions/book.ts
+++ lib/admin/actions/book.ts
@@ -1,10 +1,19 @@
 "use server";
 
 import { db } from "@/db/drizzle";
+import { books } from "@/db/schema";
 
-const createBook = async (params: BookParams) => {
+export const createBook = async (params: BookParams) => {
   try {
-    const newBook = await db.insert(books);
+    const newBook = await db
+      .insert(books)
+      .values({ ...params, availableCopies: params.totalCopies })
+      .returning();
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(newBook[0])),
+    };
   } catch (error) {
     console.log(error);
     return { success: false, message: "An error occured while creating book" };
